80e6de42c57ea8d7ea43ff8a52a0bafbbce69584
Daniel Moraes <dmrs07@gmail.com>
2026-08-31 14:02:38 -0300

    fix: bundle CommonJS dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,4 @@
+import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
@@ -10,6 +11,7 @@ export default {
   },
   plugins: [
     nodeResolve({ preferBuiltins: true }),
+    commonjs(),
     typescript({ tsconfig: "./tsconfig.json" })
   ]
 };
