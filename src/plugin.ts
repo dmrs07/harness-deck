9f89d062cb5c915098b64363cf46d86339191536
Daniel Moraes <dmrs07@gmail.com>
2026-08-31 16:01:09 -0300

    feat: register usage strip actions

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,17 @@
 import streamDeck from "@elgato/streamdeck";
-import { ClaudeUsageAction, CodexUsageAction, CombinedUsageAction } from "./actions/usage-actions.js";
+import {
+  ClaudeUsageAction,
+  ClaudeUsageStripAction,
+  CodexUsageAction,
+  CodexUsageStripAction,
+  CombinedUsageAction
+} from "./actions/usage-actions.js";
 import { usageService } from "./service/usage-service.js";
 
 streamDeck.actions.registerAction(new CodexUsageAction());
 streamDeck.actions.registerAction(new ClaudeUsageAction());
+streamDeck.actions.registerAction(new CodexUsageStripAction());
+streamDeck.actions.registerAction(new ClaudeUsageStripAction());
 streamDeck.actions.registerAction(new CombinedUsageAction());
 
 usageService.start();
